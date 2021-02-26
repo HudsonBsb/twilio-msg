6d608ec3f3fb3beece27d492a311d0945425d7ed
Hudson Henrique Queiroz Gomes <hudson.gomes@originalapp.com.br>
2021-02-26 18:05:01 -0300

    ,,.,

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ router.get('/', (req, res) => {
         .create({
             body: 'Teste de envio de mensagem node.',
             from: 'whatsapp:+14155238886',
-            to: 'whatsapp:+556181461449'
+            to: 'whatsapp:+556181461449',
+            mediaUrl: 'https://i.pinimg.com/originals/0a/1f/82/0a1f820e29719c7b67e9d5aa44241155.png'
         })
         .then(message => console.log(message.sid))
         .done();
