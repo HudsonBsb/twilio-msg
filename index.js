24250b2dddc4e9e6511b0ef551682de061fe5e0d
Hudson Henrique Queiroz Gomes <hudson.gomes@originalapp.com.br>
2021-02-26 18:36:06 -0300

    config do body

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
 const express = require('express')
 const app = express();
-const router = express.Router();
-const fs = require('fs');
 const { port, accountSid, authToken } = require('./src/config');
 const client = require('twilio')(accountSid, authToken);
-const { MessagingResponse } = require('twilio').twiml;
-const goodBoyUrl = 'https://images.unsplash.com/photo-1518717758536-85ae29035b6d?'
-  + 'ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80';
 
+app.use(express.json())
 
-client.chat.services('IS4940e4b5a0934dc8983df2df549d3acc')
-    .update({
-        webhookFilters: ['onMessageSent', 'onMessageSend', 'onMessageUpdate', 'onMessageUpdated', 'onMessageRemove', 'onMessageRemoved', 'onChannelAdd', 'onChannelAdded', 'onChannelDestroy', 'onChannelDestroyed', 'onChannelUpdate', 'onChannelUpdated', 'onMemberAdd', 'onMemberAdded', 'onMemberRemove', 'onMemberRemoved', 'onUserUpdate', 'onUserUpdated']
-    })
-    .then(service => console.log('service return console => ', service.friendlyName));
-
-router.get('/', (req, res) => {
+app.get('/', (req, res) => {
     client.messages
         .create({
             body: 'Teste de envio de mensagem node.',
@@ -28,30 +18,17 @@ router.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-router.post('/received', (req, res) => {
+app.post('/received', (req, res) => {
     const { body } = req;
-
-    let message;
-
-    if (body.NumMedia > 0) {
-        message = new MessagingResponse().message("Thanks for the image! Here's one for you!");
-        message.media(goodBoyUrl);
-    } else {
-        message = new MessagingResponse().message('Send us an image!');
-    }
-    console.log('message received message => ', message);
     console.log('body received message => ', body);
-    // console.log('req received message => ', req);
     res.send({ message: 'received' })
 })
 
-router.post('/status', (req, res) => {
+app.post('/status', (req, res) => {
     const { body } = req;
     console.log('body received message status is changed => ', body);
     res.send({ message: 'received status' })
 })
-
-app.use(router);
 
 app.listen(port, () => {
     console.log(`rodando em http://localhost:${port}`)
